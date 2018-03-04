3b3f1c414bf25a238cc7303db543d7880e97a1d9
wkwkrnht <wkwkrnht@gmail.com>
2018-03-04 22:08:33 +0900

    fix

--- main.rb
+++ main.rb
@@ -33,9 +33,8 @@ class MAIN
         tags = JSON.parse(tags.body)
         tags.each_with_index do |tag, i|
             id = tag[i]['id'].to_s
-            posts = wp_client.list_posts("wp-json/wp/v2/posts?tags=#{id}&per_page=100")
-            total_posts = posts.headers['x-wp-totalpages'].to_i
-            total_posts = total_posts * 100
+            posts = wp_client.list_posts("wp-json/wp/v2/posts?tags=#{id}&per_page=1")
+            total_posts = posts.headers['x-wp-totalpages']
             path = "wp-json/wp/v2/posts?_embed&tags=#{id}&per_page=#{total_posts}"
             body = Slim::Template.new('template/tag.html.slim').render(PASS_data.new(path))
             File.open("tags/#{id}.html","w") do |text|
