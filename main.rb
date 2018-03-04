4f0d4811b1c6b7801379be7e1b2a532223092f15
wkwkrnht <wkwkrnht@gmail.com>
2018-03-04 18:50:15 +0900

    test

--- main.rb
+++ main.rb
@@ -31,9 +31,8 @@ class MAIN
         total_tags = total_tags * 100
         tags = wp_client.list_posts("wp-json/wp/v2/tags?per_page=#{total_tags}")
         tags = JSON.parse(tags.body)
-        tags = tags[0]
-        tags.each do |tag|
-            id = tag['id'].to_s
+        tags.each_with_index do |tag, i|
+            id = tag[i]['id'].to_s
             posts = wp_client.list_posts("wp-json/wp/v2/posts?tags=#{id}&per_page=100")
             total_posts = posts.headers['x-wp-totalpages'].to_i
             total_posts = total_posts * 100
