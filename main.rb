bacf548b9f238cfb901bb32ffd2b9070571db1c8
wkwkrnht <wkwkrnht@gmail.com>
2018-03-12 21:25:51 +0900

    test

--- main.rb
+++ main.rb
@@ -31,14 +31,15 @@ class MAIN
         total_tags = total_tags.to_s
         tags = wp_client.list_posts("wp-json/wp/v2/tags?per_page=#{total_tags}")
         tags = JSON.parse(tags.body)
-        tags.each do |tag|
-            id = tag['id']
-            id = id.to_s
-            body = Slim::Template.new('template/tag.html.slim').render(PASS_data.new(id))
-            File.open("tags/#{id}.html","w") do |text|
-                text.puts(body)
-            end
-        end
+        p tags
+        #tags.each do |tag|
+        #    id = tag['id']
+        #    id = id.to_s
+        #    body = Slim::Template.new('template/tag.html.slim').render(PASS_data.new(id))
+        #    File.open("tags/#{id}.html","w") do |text|
+        #        text.puts(body)
+        #    end
+        #end
     end
 end
 
